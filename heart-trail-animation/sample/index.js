61ccefac9438daac56c785f8bc6619ac00136dd3
hoaiphu <rubik1x11@gmail.com>
2024-09-19 15:41:16 +0700

    create: heart trail animation

---
+++ heart-trail-animation/sample/index.js
@@ -0,0 +1,16 @@
+const bodyEl = document.querySelector("body");
+
+bodyEl.addEventListener("mousemove", (event)=>{
+    const xPos = event.offsetX;
+    const yPos = event.offsetY;
+    const divEl = document.createElement("div")
+    divEl.style.left = xPos + "px";
+    divEl.style.top = yPos + "px";
+    const size = Math.random() * 100;
+    divEl.style.width = size + "px";
+    divEl.style.height = size + "px";
+    bodyEl.appendChild(divEl);
+    setTimeout(() => {
+        divEl.remove();
+    }, 3000);
+});
